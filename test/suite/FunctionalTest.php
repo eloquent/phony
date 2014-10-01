6162f689205f07f6fac963947c3ffc383c11626b
Erin Millard <ezzatron@gmail.com>
2014-10-01 15:18:47 +1000

    Improved functional tests.

--- test/suite/FunctionalTest.php
+++ test/suite/FunctionalTest.php
@@ -19,19 +19,13 @@ class FunctionalTest extends PHPUnit_Framework_TestCase
         $spy('argumentA', 'argumentB', 'argumentC');
         $spy(111);
 
-        $this->assertTrue($spy->calledWith('argumentA', 'argumentB', 'argumentC'));
-        $this->assertTrue($spy->calledWith('argumentA', 'argumentB'));
-        $this->assertTrue($spy->calledWith('argumentA'));
-        $this->assertTrue($spy->calledWith());
-        $this->assertTrue($spy->calledWith(111));
-        $this->assertTrue($spy->calledWith($this->identicalTo('argumentA'), $this->anything()));
-    }
-
-    public function testCallAssertCalledWith()
-    {
-        $spy = Phony::spy();
-        $spy('argumentA', 'argumentB', 'argumentC');
-
+        $spy->assertCalledWith('argumentA', 'argumentB', 'argumentC');
+        $spy->assertCalledWith('argumentA', 'argumentB');
+        $spy->assertCalledWith('argumentA');
+        $spy->assertCalledWith();
+        $spy->assertCalledWith(111);
+        $spy->assertCalledWith($this->identicalTo('argumentA'), $this->anything());
         $spy->callAt(0)->assertCalledWith('argumentA', 'argumentB', 'argumentC');
+        $spy->callAt(1)->assertCalledWith(111);
     }
 }
