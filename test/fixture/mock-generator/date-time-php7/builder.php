00993d6e23b4d3df88162fcec87581fa5bede967
Erin Millard <ezzatron@gmail.com>
2015-07-17 22:24:44 +1000

    Remove test for PHP7 interface in flux.

--- test/fixture/mock-generator/date-time-php7/builder.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return new Eloquent\Phony\Mock\Builder\MockBuilder(
-    'DateTime',
-    null,
-    'MockGeneratorDateTime'
-);
--- test/fixture/mock-generator/date-time-php7/supported.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$message = 'Requires non-HHVM runtime (PHP 7).';
-
-return $detector->isSupported('runtime.php') &&
-    version_compare(PHP_VERSION, '6.999', '>');
