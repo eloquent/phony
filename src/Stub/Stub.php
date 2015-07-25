b325f5881082f276deff250d62b5f1d56fe8f04a
Erin Millard <ezzatron@gmail.com>
2015-07-25 16:08:31 +1000

    Allow throws() to accept strings. Closes #76.

--- src/Stub/Stub.php
+++ src/Stub/Stub.php
@@ -564,12 +564,12 @@ class Stub extends AbstractWrappedInvocable implements StubInterface
     /**
      * Add an answer that throws an exception.
      *
-     * @param Exception|null $exception                The exception, or null to throw a generic exception.
-     * @param Exception      $additionalExceptions,... Additional exceptions for subsequent invocations.
+     * @param Exception|string|null $exception                The exception, or message, or null to throw a generic exception.
+     * @param Exception|string      $additionalExceptions,... Additional exceptions, or messages, for subsequent invocations.
      *
      * @return StubInterface This stub.
      */
-    public function throws(Exception $exception = null)
+    public function throws($exception = null)
     {
         if (0 === func_num_args()) {
             return $this->doesWith(
@@ -584,6 +584,10 @@ class Stub extends AbstractWrappedInvocable implements StubInterface
         }
 
         foreach (func_get_args() as $exception) {
+            if (!$exception instanceof Exception) {
+                $exception = new Exception(strval($exception));
+            }
+
             $this->doesWith(
                 function () use ($exception) {
                     throw $exception;
--- test/suite/Stub/StubVerifierTest.php
+++ test/suite/Stub/StubVerifierTest.php
@@ -895,6 +895,22 @@ class StubVerifierTest extends PHPUnit_Framework_TestCase
         $this->assertSame(array($exceptionA, $exceptionB, $exceptionB), $thrownExceptions);
     }
 
+    public function testThrowsWithMessage()
+    {
+        $this->assertSame($this->subject, $this->subject->throws('a', 'b'));
+
+        $thrownExceptions = array();
+        for ($i = 0; $i < 3; ++$i) {
+            try {
+                call_user_func($this->subject);
+            } catch (Exception $thrownException) {
+                $thrownExceptions[] = $thrownException;
+            }
+        }
+
+        $this->assertEquals(array(new Exception('a'), new Exception('b'), new Exception('b')), $thrownExceptions);
+    }
+
     public function testMultipleRules()
     {
         $this->assertSame(
