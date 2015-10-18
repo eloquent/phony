9c14a27993adc28f3b14384d48a442988d74beb9
James Harris <james.harris@icecave.com.au>
2015-10-18 13:53:10 +1000

    Reimplemenated EqualToMatcher as an iterative, deep, strict comparator.

---
+++ test/benchmarks/Matcher/EqualToMatcherVsRecursiveEvent.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * This file is part of the Phony package.
+ *
+ * Copyright © 2015 Erin Millard
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+namespace Eloquent\Phony\Matcher;
+
+use Athletic\AthleticEvent;
+
+class EqualToMatcherVsRecursiveEvent extends AthleticEvent
+{
+    protected function generateNestedArray($depth)
+    {
+        $array = array('end');
+
+        for ($i = 0; $i < $depth; ++$i) {
+            $array = array('nested' => $array);
+        }
+
+        return $array;
+    }
+
+    protected function setUp()
+    {
+        require_once __DIR__ . '/RecursiveEqualToMatcher.php';
+
+        // Depth must be lower than PHP's stack depth.
+        $this->array1 = $this->generateNestedArray(200);
+        $this->array2 = $this->generateNestedArray(200);
+
+        $this->iterative = new EqualToMatcher($this->array1);
+        $this->recursive = new RecursiveEqualToMatcher($this->array1);
+    }
+
+    /**
+     * @iterations 10
+     */
+    public function iterative()
+    {
+        $this->iterative->matches(
+            $this->array2
+        );
+    }
+
+    /**
+     * @iterations 10
+     */
+    public function recursive()
+    {
+        $this->recursive->matches(
+            $this->array2
+        );
+    }
+}
---
+++ test/src/Test/Matcher/TestDerivedClassWithTraitA.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the Phony package.
+ *
+ * Copyright © 2015 Erin Millard
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+namespace Eloquent\Phony\Test\Matcher;
+
+class TestDerivedClassWithTraitA extends TestBaseClass
+{
+    use TestTrait;
+
+    public function __construct(
+        $publicValue = null,
+        $privateValue = null,
+        $protectedValue = null
+    ) {
+        if (null !== $publicValue) {
+            $this->derivedPublic = $publicValue;
+        }
+        if (null !== $privateValue) {
+            $this->derivedPrivate = $privateValue;
+        }
+        if (null !== $protectedValue) {
+            $this->derivedProtected = $protectedValue;
+        }
+    }
+
+    public $derivedPublic = '<derived-public>';
+    private $derivedPrivate = '<derived-private>';
+    protected $derivedProtected = '<derived-protected>';
+}
