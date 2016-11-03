7266e3fc4fb6a422feb04eca8fa65c25c4465db7
Woody Gilk <woody.gilk@gmail.com>
2016-11-03 13:15:50 -0500

    Fix fatal error on HHVM with nullable type
    HHVM does not support the nullable type outside of Hack code.
    
    Broken by 6bf897cb2215ff209ba5e2794c55faaa63d6b4b4
    
    Fixes #203

--- src/Reflection/FeatureDetector.php
+++ src/Reflection/FeatureDetector.php
@@ -440,6 +440,11 @@ class FeatureDetector
             },
 
             'type.nullable' => function ($detector) {
+                // syntax causes fatal on HHVM
+                if ($detector->isSupported('runtime.hhvm')) {
+                    return false; // @codeCoverageIgnore
+                }
+
                 return $detector->checkStatement(
                     sprintf(
                         'function(?int $a){}',
