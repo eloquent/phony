b7b38c08ebb2094833892b9d9ba80c183fa9bb8b
Erin Millard <ezzatron@gmail.com>
2017-04-23 12:44:24 +1000

    Bump copyright year.

--- src/Spy/TraversableSpy.php
+++ src/Spy/TraversableSpy.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Phony package.
  *
- * Copyright © 2016 Erin Millard
+ * Copyright © 2017 Erin Millard
  *
  * For the full copyright and license information, please view the LICENSE file
  * that was distributed with this source code.
