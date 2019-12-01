9b9c9e4b86b82f83299bedef39706ee175912685
Erin Millard <ezzatron@gmail.com>
2019-12-01 11:31:37 +1000

    Fix router edge-cases

--- assets/router.php
+++ assets/router.php
@@ -1,13 +1,13 @@
 <?php
 
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$file = basename($path);
 
-if (false !== strpos($file, '.')) {
+if (preg_match('/[^.]*[a-z]+[^.]*\./', basename($path))) {
     return false;
 }
 
-$content = @file_get_contents($_SERVER['DOCUMENT_ROOT'] . $path . '.html');
+$htmlPath = '/' === substr($path, -1) ? "$path/index.html" : "$path.html";
+$content = @file_get_contents($_SERVER['DOCUMENT_ROOT'] . $htmlPath);
 
 if (false === $content) {
     return false;
