cd29230c369b5e7ab0c0e9bd937d7ff7350f87a2
Erin Millard <ezzatron@gmail.com>
2019-12-28 15:01:50 +1000

    Added missing generator verification tests

---
+++ test/fixture/verification/received-exception/call-never-any/verification.php
@@ -0,0 +1,17 @@
+<?php
+
+use Eloquent\Phony\Test\Phony;
+
+// setup
+$stub = Phony::stub()->setLabel('label')->setUseIterableSpies(true);
+$stub->with('aardvark')->returns(['AARDVARK']);
+$stub->with('bonobo')->generates(['BONOBO', 'BADGER']);
+iterator_to_array($stub('aardvark'));
+$generator = $stub('bonobo');
+try {
+    $generator->throw(new RuntimeException('MECHA-BONOBO'));
+} catch (RuntimeException $e) {
+}
+
+// verification
+$stub->lastCall()->generated()->never()->receivedException();
